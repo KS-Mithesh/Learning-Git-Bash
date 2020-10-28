3d453345578c849c1a2b4bea7f677764851a6aaa
KS-Mithesh <72862256+KS-Mithesh@users.noreply.github.com>
2020-10-28 16:45:38 +0530

    Add rav

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var rav;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +16,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    rav = new Ground(600,350,1200,20);
+
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -46,6 +48,7 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
+    rav.display();
     pig1.display();
     log1.display();
 
